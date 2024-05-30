fe3572ccfe3c5ba6dbafaf53f17e8d38e0f8122a
San Valen <osoriovalentin5@gmail.com>
2024-05-30 17:40:43 -0500

    Lint all files

--- db/scripts/migrate.ts
+++ db/scripts/migrate.ts
@@ -1,17 +1,17 @@
-import { Client } from "pg"
-import { drizzle } from "drizzle-orm/node-postgres"
-import * as Schema from 'src/drizzle/schema'
-import { envs } from "src/config"
+import { Client } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import * as Schema from 'src/drizzle/schema';
+import { envs } from 'src/config';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
 export async function migrateDb() {
   const client = new Client({
     connectionString: envs.DATABASE_URL,
-  })
+  });
 
   await client.connect();
 
-  const db = drizzle(client, { schema: Schema })
+  const db = drizzle(client, { schema: Schema });
 
   await migrate(db, { migrationsFolder: './drizzle' });
 
@@ -21,4 +21,4 @@ export async function migrateDb() {
 migrateDb()
   .then(() => console.log('Migrations complete'))
   .catch((err) => console.error(err))
-  .finally(() => process.exit(0))
+  .finally(() => process.exit(0));
