d5f39e7800b4153b628f25a5084417cd116de889
San Valen <osoriovalentin5@gmail.com>
2024-06-01 12:58:27 -0500

    Remove unecesary package @nestjs/swagger

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 import { IsNumber, IsPositive } from 'class-validator';
 
